b6567769f9f3eb699680a4f2219b9024269cf81f
Sergioaquiles <sergioaquiles@hotmail.com>
2022-03-06 23:31:44 -0300

    Initial Commit

---
+++ AppStoreViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreViewCode
+//
+//  Created by Sergio Cardoso on 06/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
